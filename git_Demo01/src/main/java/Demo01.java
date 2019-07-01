ab6ed682c3d3d3336a360b939ae83e8694c767c8
luliang <1575190048@qq.com>
2019-07-01 23:04:30 +0800

    新建的git项目，提交到本地创库

---
+++ git_Demo01/src/main/java/Demo01.java
@@ -0,0 +1,11 @@
+/**
+ * @program:GITXiangMu
+ * @description:鲁亮
+ * @author:Mr.lu
+ * @create:2019-07-01 22:59
+ **/
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+    }
+}
